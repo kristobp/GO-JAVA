c30fd3eb7f81a49a0bc754b043ad5308dfed335e
Irsyah Mardiah (Icha) <icha-hacktiv8@Irsyahs-MacBook-Air.local>
2021-07-02 10:32:41 +0700

    use bootstrap

--- script.js
+++ script.js
@@ -241,201 +241,253 @@ const budgets = {
 
 // Display destinasi //
 
-const kontainerDestinasi = document.querySelector('.destinasi-box');
-console.log(kontainerDestinasi.innerHTML);
-
-// get user budget input
-const getIndex = document.querySelectorAll('.budget');
-// gimana cara tau kalo user pilih opsi itu ???
-const inputBudget = document.querySelector('#budget-user');
-const getUserBudget = function (budgets, getIndex) {
-  // TODO: RETURN false IF buat kondisi kalo isinya "Pilih Range Harga"
-  for (el of getIndex) {
-    const budgetUser = el.className[el.className.length - 1];
-
-    const min = budgets[budgetUser].min;
-    const max = budgets[budgetUser].max;
-
-    return [min, max];
-  }
-};
-let rangeBudget = [0, 100000];
-// rangeBudget = false;
-// let rangeBudget = getUserBudget(budgets, getIndex);
-// console.log(rangeBudget);
-
-// sorting destinasi by harga per kategori
-const sorting = function (desKat) {
-  const sortedDestinasi = [];
-  const arr = [];
-  const index = [];
-  for ([i, el] of desKat.entries()) {
-    arr.push(el.harga);
-    index.push(i);
-  }
-  for (el of arr) {
-    for (let i = 0; i < arr.length; i++) {
-      let tempEl = arr[i + 1];
-      let tempIn = index[i + 1];
-      if (arr[i] > tempEl) {
-        arr[i + 1] = arr[i];
-        index[i + 1] = index[i];
-        arr[i] = tempEl;
-        index[i] = tempIn;
-      }
-    }
-  }
-  for ([i, el] of index.entries()) {
-    sortedDestinasi.push(desKat[index[i]]);
-  }
-  return sortedDestinasi;
-};
+// const kontainerDestinasi = document.querySelector('.destinasi-box');
+// console.log(kontainerDestinasi.innerHTML);
+//
+// // get user budget input
+// const getIndex = document.querySelectorAll('.budget');
+// // gimana cara tau kalo user pilih opsi itu ???
+// const inputBudget = document.querySelector('#budget-user');
+// const getUserBudget = function (budgets, getIndex) {
+//   // TODO: RETURN false IF buat kondisi kalo isinya "Pilih Range Harga"
+//   for (el of getIndex) {
+//     const budgetUser = el.className[el.className.length - 1];
+//
+//     const min = budgets[budgetUser].min;
+//     const max = budgets[budgetUser].max;
+//
+//     return [min, max];
+//   }
+// };
+// let rangeBudget = [0, 100000];
+// // rangeBudget = false;
+// // let rangeBudget = getUserBudget(budgets, getIndex);
+// // console.log(rangeBudget);
+//
+// // sorting destinasi by harga per kategori
+// const sorting = function (desKat) {
+//   const sortedDestinasi = [];
+//   const arr = [];
+//   const index = [];
+//   for ([i, el] of desKat.entries()) {
+//     arr.push(el.harga);
+//     index.push(i);
+//   }
+//   for (el of arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//       let tempEl = arr[i + 1];
+//       let tempIn = index[i + 1];
+//       if (arr[i] > tempEl) {
+//         arr[i + 1] = arr[i];
+//         index[i + 1] = index[i];
+//         arr[i] = tempEl;
+//         index[i] = tempIn;
+//       }
+//     }
+//   }
+//   for ([i, el] of index.entries()) {
+//     sortedDestinasi.push(desKat[index[i]]);
+//   }
+//   return sortedDestinasi;
+// };
 
 // create list
 const createList = function (destinasiReal, rangeBudget) {
-  // let divDestinationBox = document.createElement()
-
+  const mainList = document.getElementById('main');
+  mainList.innerHTML = '';
 
   for (desti of destinasiReal) {
+      let divCard = document.createElement('div');
+      divCard.classList.add('destinasi-box');
+      divCard.setAttribute('style', 'width: 18rem;');
 
-    if (desti.harga >= rangeBudget[0] && desti.harga <= rangeBudget[1]) {
-      const html = `
-          <div class="${kategori}">
-          <div class="nama">${desti.nama}</div>
-          <div class="lokasi">${desti.lokasi}</div>
-          <div class="kuota">${desti.kuota}</div>
-          <div class="harga">${desti.harga}</div>
-          <div class="rating">${desti.rating}</div>
-          <div class="gambar">${desti.gambar}</div>
-          </div>
-          `;
-      kontainerDestinasi.insertAdjacentHTML('beforebegin', html);
-    }
+      let image = document.createElement('img');
+      image.setAttribute("src",desti.gambar);
+      image.classList.add('card-img-top');
+
+      let divBody = document.createElement('div');
+      divBody.classList.add('card-body');
+
+      let dest = document.createElement('h5');
+      dest.innerHTML = desti.nama;
+      divBody.appendChild(dest);
+
+
+      divCard.appendChild(image);
+      divCard.appendChild(divBody);
+      mainList.appendChild(divCard);
+    // }
   }
 };
 
-const displayDestinasi = function (
-  destinasi,
-  rangeBudget,
-  filter = false,
-  sorted = false
-) {
-  // Check budget
-  if (!rangeBudget) {
-    alert('Duit lu berapa neh? Pilih dulu ea');
-    return;
-  }
-  kontainerDestinasi.innerHTML = '';
-  console.log(kontainerDestinasi.innerHTML);
 
-  for (kategori in destinasi) {
-    // Check filter
-    if (filter) {
-      kategori = filter;
-      // Check sorted
-      const destinasiReal = sorted
-        ? sorting(destinasi[kategori].slice())
-        : destinasi[kategori].slice();
-      createList(destinasiReal, rangeBudget);
-      break;
-    } else {
-      // Check sorted
-      const destinasiReal = sorted
-        ? sorting(destinasi[kategori].slice())
-        : destinasi[kategori].slice();
-      createList(destinasiReal, rangeBudget);
-    }
-  }
-};
+let data = [
+  {
+    nama: `Bromo`,
+    lokasi: `Jawa Timur`,
+    kuota: 20,
+    harga: 200000,
+    rating: 4.6,
+    gambar: `https://bit.ly/2Uhro2c`,
+  },
+  {
+    nama: `Semeru`,
+    lokasi: `Jawa Timur`,
+    kuota: 30,
+    harga: 150000,
+    rating: 4.6,
+    gambar: `https://bit.ly/3jyXJMv`,
+  },
+  {
+    nama: `Merapi`,
+    lokasi: `Jawa Tengah`,
+    kuota: 25,
+    harga: 100000,
+    rating: 4.4,
+    gambar: `https://bit.ly/3dxRBAi`,
+  },
+  {
+    nama: `Merbabu`,
+    lokasi: `Jawa Tengah`,
+    kuota: 40,
+    harga: 250000,
+    rating: 4.6,
+    gambar: `https://bit.ly/3jF6AMG`,
+  },
+  {
+    nama: `Galunggung`,
+    lokasi: `Jawa Barat`,
+    kuota: 35,
+    harga: 100000,
+    rating: 4.4,
+    gambar: `https://bit.ly/366MJOM`,
+  },
+]
 
-// Update UI //
+createList(data, 2000);
 
-// BUG masih nimpa
-const updateUI = function (
-  destinasi,
-  rangeBudget,
-  filter = false,
-  sorted = false
-) {
-  kontainerDestinasi.innerHTML = ''; //BUG
-  // console.log(kontainerDestinasi.innerHTML);
-
-  displayDestinasi(destinasi, rangeBudget, filter, sorted);
-};
-
-// Search & Reset buttons //
-
-const btnSearch = document.querySelector('.search-btn');
-const btnReset = document.querySelector('.reset-btn');
-
-let dataUser = [];
-const inputName = document.querySelector('#nama-user');
-const inputEmail = document.querySelector('#email-user');
-
-btnSearch.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (!inputName.value || !inputEmail.value) {
-    alert('Aelah, Masukin nama dan email dong coy');
-    return;
-  }
-  dataUser.push(inputName.value, inputEmail.value);
-
-  updateUI(destinasi, rangeBudget);
-});
-
-btnReset.addEventListener('click', function (e) {
-  inputName.value = '';
-  inputEmail.value = '';
-  inputBudget.value = '';
-  dataUser = [];
-});
+// const displayDestinasi = function (
+//   destinasi,
+//   rangeBudget,
+//   filter = false,
+//   sorted = false
+// ) {
+//   // Check budget
+//   if (!rangeBudget) {
+//     alert('Duit lu berapa neh? Pilih dulu ea');
+//     return;
+//   }
+//   kontainerDestinasi.innerHTML = '';
+//   console.log(kontainerDestinasi.innerHTML);
+//
+//   for (kategori in destinasi) {
+//     // Check filter
+//     if (filter) {
+//       kategori = filter;
+//       // Check sorted
+//       const destinasiReal = sorted
+//         ? sorting(destinasi[kategori].slice())
+//         : destinasi[kategori].slice();
+//       createList(destinasiReal, rangeBudget);
+//       break;
+//     } else {
+//       // Check sorted
+//       const destinasiReal = sorted
+//         ? sorting(destinasi[kategori].slice())
+//         : destinasi[kategori].slice();
+//       createList(destinasiReal, rangeBudget);
+//     }
+//   }
+// };
+//
+// // Update UI //
+//
+// // BUG masih nimpa
+// const updateUI = function (
+//   destinasi,
+//   rangeBudget,
+//   filter = false,
+//   sorted = false
+// ) {
+//   kontainerDestinasi.innerHTML = ''; //BUG
+//   // console.log(kontainerDestinasi.innerHTML);
+//
+//   displayDestinasi(destinasi, rangeBudget, filter, sorted);
+// };
+//
+// // Search & Reset buttons //
+//
+// const btnSearch = document.querySelector('.search-btn');
+// const btnReset = document.querySelector('.reset-btn');
+//
+// let dataUser = [];
+// const inputName = document.querySelector('#nama-user');
+// const inputEmail = document.querySelector('#email-user');
+//
+// btnSearch.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   if (!inputName.value || !inputEmail.value) {
+//     alert('Aelah, Masukin nama dan email dong coy');
+//     return;
+//   }
+//   dataUser.push(inputName.value, inputEmail.value);
+//
+//   updateUI(destinasi, rangeBudget);
+// });
+//
+// btnReset.addEventListener('click', function (e) {
+//   inputName.value = '';
+//   inputEmail.value = '';
+//   inputBudget.value = '';
+//   dataUser = [];
+// });
 
 // Filter buttons //
 
-let filter = false;
-
-const btnGunung = document.querySelector('.gunung-btn');
-const btnPantai = document.querySelector('.pantai-btn');
-const btnMuseum = document.querySelector('.museum-btn');
-const btnTaman = document.querySelector('.taman-rekreasi-btn');
-const btnKebun = document.querySelector('.kebun-binatang-btn');
-
-btnPantai.addEventListener('click', function (e) {
-  filter = 'pantai';
-  updateUI(destinasi, rangeBudget, filter);
-  filter = false;
-});
-btnGunung.addEventListener('click', function (e) {
-  filter = 'gunung';
-  updateUI(destinasi, rangeBudget, filter);
-  filter = false;
-});
-btnMuseum.addEventListener('click', function (e) {
-  filter = 'museum';
-  updateUI(destinasi, rangeBudget, filter);
-  filter = false;
-});
-btnTaman.addEventListener('click', function (e) {
-  filter = 'tamanRekreasi';
-  updateUI(destinasi, rangeBudget, filter);
-  filter = false;
-});
-btnKebun.addEventListener('click', function (e) {
-  filter = 'kebunBinatang';
-  updateUI(destinasi, rangeBudget, filter);
-  filter = false;
-});
-
-// Sort button //
-
-let sorted = false;
-
-const btnSort = document.querySelector('.sorting-btn');
-
-btnSort.addEventListener('click', function (e) {
-  updateUI(destinasi, rangeBudget, filter, !sorted);
-  sorted = !sorted;
-});
+// let filter = false;
+//
+// const btnGunung = document.querySelector('.gunung-btn');
+// const btnPantai = document.querySelector('.pantai-btn');
+// const btnMuseum = document.querySelector('.museum-btn');
+// const btnTaman = document.querySelector('.taman-rekreasi-btn');
+// const btnKebun = document.querySelector('.kebun-binatang-btn');
+//
+// btnPantai.addEventListener('click', function (e) {
+//   filter = 'pantai';
+//   updateUI(destinasi, rangeBudget, filter);
+//   filter = false;
+// });
+// btnGunung.addEventListener('click', function (e) {
+//   filter = 'gunung';
+//   updateUI(destinasi, rangeBudget, filter);
+//   filter = false;
+// });
+// btnMuseum.addEventListener('click', function (e) {
+//   filter = 'museum';
+//   updateUI(destinasi, rangeBudget, filter);
+//   filter = false;
+// });
+// btnTaman.addEventListener('click', function (e) {
+//   filter = 'tamanRekreasi';
+//   updateUI(destinasi, rangeBudget, filter);
+//   filter = false;
+// });
+// btnKebun.addEventListener('click', function (e) {
+//   filter = 'kebunBinatang';
+//   updateUI(destinasi, rangeBudget, filter);
+//   filter = false;
+// });
+//
+// // Sort button //
+//
+// let sorted = false;
+//
+// const btnSort = document.querySelector('.sorting-btn');
+//
+// btnSort.addEventListener('click', function (e) {
+//   updateUI(destinasi, rangeBudget, filter, !sorted);
+//   sorted = !sorted;
+// });
 
 ///////////////////////////////////////////////////////
 // NOTE: belum dikerjain, belum ada HTML untuk skenario slanjutnya
